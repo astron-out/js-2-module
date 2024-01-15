8eeff3d6fa3b1e1f6b74e870535534086ba45af5
astron-out <cosmos.4618282@gmail.com>
2024-01-15 22:12:49 +0500

    Introdusing functions

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive :D");
+
+// Functions
+
+function logger() {
+  console.log("I'm Khudoyberdi");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 3);
+console.log(appleJuice);
