786b845491e410cc499837a3a4370e0f69cbd0c7
astron-out <cosmos.4618282@gmail.com>
2024-01-17 19:58:06 +0500

    Arrow functions and Function calling another function

--- script.js
+++ script.js
@@ -1,26 +1,72 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D");
 
-// Functions
+// // Functions
 
-function logger() {
-  console.log("I'm Khudoyberdi");
-}
+// function logger() {
+//   console.log("I'm Khudoyberdi");
+// }
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 3);
+// console.log(appleJuice);
+
+// Function Decloration
+// function calcAge1(birthYear) {
+//   const age = 2024 - birthYear;
+//   return age;
+// }
+
+// const age1 = calcAge1(2007);
+// console.log(age1);
+
+// Function Expression
+// const calcAge2 = function (birthYear) {
+//   return 2024 - birthYear;
+// };
+
+// const age2 = calcAge2(2007);
+// console.log(age1, age2);
+
+// Arrow function
+
+// const calcAge3 = (birthYear) => 2024 - birthYear;
+
+// const age = calcAge3(2007);
+// console.log(age);
+
+// const yearsUntilRetirment = (birthYear, firstName, lastName) => {
+//   const age1 = 2024 - birthYear;
+//   const retirment = 60 - age1;
+//   return `${firstName} ${lastName} retires in ${retirment} years`;
+// };
+
+// console.log(yearsUntilRetirment(2007, "Khudoyberdi", "Shamsiyev"));\
+
+const cutPieces = function (fruit) {
+  return fruit * 4;
+};
+
+const fruitProcessor = function (apples, oranges) {
+  const applePieces = cutPieces(apples);
+  const orangePieces = cutPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and with ${orangePieces} pieces of orange`;
   return juice;
-}
+};
 
-const appleJuice = fruitProcessor(5, 3);
-console.log(appleJuice);
+console.log(fruitProcessor(2, 3));
